fc42e83c4efbab543a4a2beb6ec7dc08805067d0
m.zanko <Zanko98@mail.ru>
2023-04-05 16:09:30 +0300

    Reformat code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         do {
-                System.out.println("* * *  Введите номер алгиритма: * * *\n 1-ввод числа,\n 2-ввод имени,\n 3-ввод числового массива, \n 0-ВЫХОД");
-                Scanner sc = new Scanner(System.in);
-                int alg = sc.nextInt();
-                if (alg == 0) break;
-                else if (alg == 1) enterNumber();
-                else if (alg == 2) enterName();
-                else if (alg == 3) enterArray();
-                else {
-                    System.out.println("* Введен неверный номер алгоритма. Повторите попытку *");
-                }
+            System.out.println("* * *  Введите номер алгиритма: * * *\n 1-ввод числа,\n 2-ввод имени,\n 3-ввод числового массива, \n 0-ВЫХОД");
+            Scanner sc = new Scanner(System.in);
+            int alg = sc.nextInt();
+            if (alg == 0) break;
+            else if (alg == 1) enterNumber();
+            else if (alg == 2) enterName();
+            else if (alg == 3) enterArray();
+            else System.out.println("* Введен неверный номер алгоритма. Повторите попытку *");
         } while (true);
     }
 
@@ -36,7 +34,7 @@ public class Main {
     public static void enterArray() {
         System.out.println("__ Введите колличество элементов массива __");
         Scanner sc = new Scanner(System.in);
-        int [] mass = new int[sc.nextInt()];
+        int[] mass = new int[sc.nextInt()];
         System.out.println("__ Введите элементы массива __");
         for (int i = 0; i < mass.length; i++) {
             mass[i] = sc.nextInt();
